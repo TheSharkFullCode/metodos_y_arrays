10e0ad5b3312a7eccc326e1c1b17afbf47a864c4
TheSharkFullCode <agedjoscar@hotmail.com>
2023-07-03 14:26:54 +0200

    update

---
+++ main.js
@@ -0,0 +1,42 @@
+// alert("hola mundo")
+// Ejercicio 1 crea una variable hacker
+let hacker1 = "oscar";
+console.log(`The name drive's is:${hacker1}`);
+
+let hacker2 =["pedro","maria","juan"];
+console.log(`The navigator names is: ${hacker2}`);
+
+                
+console.log("------inteacion 2 condicionalesl-----");
+
+console.log(hacker2);
+
+console.log(`It seem the navigator  has the longer names : ${hacker2}`);
+
+console.log(`-----------imprime todo los caracteres del name conductor---`);
+
+
+console.log( `iteracion 3`);
+// imprime todo los caracteres de el nombre del conductor, seprado por espacio.
+let namesdriver = hacker1.split('');
+console.log(namesdriver);
+// imprime todo los caracteres del navegante in reverse
+let changeOrden = hacker2.reverse();
+console.log(changeOrden);
+//dependiendo del lexico imprimir:¿cual va primero?
+
+
+
+//Bonus times 1..
+// Ir al generador lorem ipsus y:
+// generar 3 parrafos. almacenar en el texto una variable de tipo cadena.
+// has que tu programa cuente el numero de palabras de tipo cadena.
+//has que tu programa cuente el nuemero de veces que aparece la palabra (et).
+
+let stringRandom1;
+
+
+
+//Bonus times 2..
+// create a new variable phrasetoCheack y que este tenga algun valro string.
+// hay que hacer que si el valor que asignamos a esta variable es un palindromo?
